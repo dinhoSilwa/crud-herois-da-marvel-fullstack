a7cdf0dfd71b49c0e9447bc262be735c6aa556e8
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-28 11:29:56 -0300

    FEAT : create api hash

--- src/api/createhash.ts
+++ src/api/createhash.ts
@@ -1,3 +1,4 @@
 export const privateKey = import.meta.env.VITE_PRIVATE_KEY
 export const publicKey = import.meta.env.VITE_PUBLIC_KEY
 export const ts = new Date().getTime();
+export createHash = ts + privateKey + publicKey;
