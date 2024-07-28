e1be440254e82088f426d98bbdb1975f30cef7b8
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-28 11:47:48 -0300

    FEAT : implemented hashing for the API parameters

--- src/api/createhash.ts
+++ src/api/createhash.ts
@@ -1,4 +1,6 @@
+import { MD5 } from "crypto-js";
 export const privateKey = import.meta.env.VITE_PRIVATE_KEY
 export const publicKey = import.meta.env.VITE_PUBLIC_KEY
-export const ts = new Date().getTime();
-export createHash = ts + privateKey + publicKey;
+export const ts = new Date().getTime().toString();
+export const createHash = ts + privateKey + publicKey;
+export const apihash = MD5(createHash).toString()
