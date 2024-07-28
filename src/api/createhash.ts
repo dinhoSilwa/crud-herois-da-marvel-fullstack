4fd9484a57634572f693748334f7aa4ba6f2d18f
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-07-28 12:20:37 -0300

    FIX : declarations

--- src/api/createhash.ts
+++ src/api/createhash.ts
@@ -1,6 +1,12 @@
 import { MD5 } from "crypto-js";
+
 export const privateKey = import.meta.env.VITE_PRIVATE_KEY
 export const publicKey = import.meta.env.VITE_PUBLIC_KEY
+
+if(!import.meta.env.VITE_PUBLIC_KEY || !import.meta.env.VITE_PRIVATE_KEY){
+  throw new Error("As variáveis de ambientes não estão definidas corretamente")
+}
+
 export const ts = new Date().getTime().toString();
 export const createHash = ts + privateKey + publicKey;
 export const apihash = MD5(createHash).toString()
